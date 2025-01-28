d2a79a9272ab23bdda097953870ed2c44c56a342
Gonzalo_Palma <gonzalopalmadev0210@gmail.com>
2025-01-27 22:32:38 -0500

    avanzando con la sección 1

---
+++ scripts/sliders.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper_images_section_1', {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+    spaceBetween: 10,
+});
