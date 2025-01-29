d46431745f0cbf42e054e984a57070b8c511f098
Gonzalo_Palma <gonzalopalmadev0210@gmail.com>
2025-01-28 22:17:37 -0500

    avanzado hasta la seccion de aliados

--- scripts/sliders.js
+++ scripts/sliders.js
@@ -12,3 +12,48 @@ const swiper = new Swiper('.swiper_images_section_1', {
         prevEl: '.swiper-atras-flecha',
     }
 });
+
+const swiper_2 = new Swiper('#swiper_slide2', {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+    spaceBetween: 15,
+    navigation: {
+        nextEl: '.slide2_flecha_siguiente',
+        prevEl: '.slide2_flecha_atras',
+    }
+});
+
+const swiper_3 = new Swiper('#swiper_slide3', {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+    spaceBetween: 15,
+    navigation: {
+        nextEl: '.flecha_slider3_siguiente',
+        prevEl: '.flecha_slider3_atras',
+    }
+});
+
+const swiper_4 = new Swiper('#swiper4', {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+    spaceBetween: 50,
+    navigation: {
+        nextEl: '.icono_flecha_slider4_siguiente',
+        prevEl: '.icono_flecha_slider4_atras',
+    }
+});
